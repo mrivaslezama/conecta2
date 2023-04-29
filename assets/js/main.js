2eab7d62870b2069ca52e702f98e8ee7d097e168
Miguel Rivas <mrivaslezama@gmail.com>
2023-04-28 23:03:54 -0400

    probando images and navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -205,4 +205,23 @@
 
 					});
 
+/* Navbarjs*/
+
+					$('.toggle').click(function () {
+						"use strict";
+						$('nav ul').slideToggle();
+					});
+					
+					
+					
+					$(window).resize(function () {
+						"use strict";
+						if ($(window).width() > 780) {
+							$('nav ul').removeAttr('style');
+						}
+					});
+					
+
+
+
 })(jQuery);
