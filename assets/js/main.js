8f195c6cbbd4fe24f63bafe7ff7ef160a09124f2
Miguel Rivas <mrivaslezama@gmail.com>
2023-04-29 12:20:09 -0400

    adding css

--- assets/js/main.js
+++ assets/js/main.js
@@ -205,23 +205,5 @@
 
 					});
 
-/* Navbarjs*/
-
-					$('.toggle').click(function () {
-						"use strict";
-						$('nav ul').slideToggle();
-					});
-					
-					
-					
-					$(window).resize(function () {
-						"use strict";
-						if ($(window).width() > 780) {
-							$('nav ul').removeAttr('style');
-						}
-					});
-					
-
-
 
 })(jQuery);
